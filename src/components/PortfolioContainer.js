f20cf9e2fc442f3e5323942b9a1a7e7e77d051ff
gallaghj13 <gallagherj0913@gmail.com>
2022-02-02 14:24:21 -0700

    added routes

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,38 +1,28 @@
 import React, { useState } from 'react';
-import NavBar from './NavBar';
 import About from './pages/About';
-// import Contact from './pages/Contact';
+import Contact from './pages/Contact';
 import Projects from './pages/Projects';
-// import Resume from './pages/Resume';
+import Resume from './pages/Resume';
+import Header from './Header';
+import Footer from './Footer';
 import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('About');
-  
-    // const renderPage = () => {
-    //   if (currentPage === 'About') {
-    //     return <About />;
-    //   }
-    //   if (currentPage === 'Project') {
-    //     return <Projects />;
-    //   }
-    //   if (currentPage === 'Resume') {
-    //     return <Resume />;
-    //   }
-    //   return <Contact />;
-    // };
   
     const handlePageChange = (page) => setCurrentPage(page);
   
     return (
     <Router>
       <div>
-        <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+        <Header currentPage={currentPage} handlePageChange={handlePageChange} />
         <Routes>
             <Route path='/projects' element= {<Projects/>} />
             <Route path='/about' element= {<About/>} />
+            <Route path='/contact' element= {<Contact/>} />
+            <Route path='/resume' element= {<Resume/>} />
         </Routes>
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </Router>
     );
