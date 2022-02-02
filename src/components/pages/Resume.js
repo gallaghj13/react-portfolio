a05b0892fb615c32c326f2b9d1aa32d26d379e7f
gallaghj13 <gallagherj0913@gmail.com>
2022-02-02 14:26:54 -0700

    just returning one line for now

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import '../../styles/Resume.css';
+
+function Resume() {
+    return (
+        <div className="resumeContainer">
+            <p> This is my Resume </p>
+        </div>
+    )
+
+}
+
+export default Resume;
